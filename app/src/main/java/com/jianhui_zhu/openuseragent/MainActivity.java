f82019c8973509b5c6534df92ec0fdb3b1fa13c9
Jianhui Zhu <jianhuizhu@JianhuideMacBook-Pro.local>
2016-01-27 15:15:54 -0500

    Initial commit

---
+++ app/src/main/java/com/jianhui_zhu/openuseragent/MainActivity.java
@@ -0,0 +1,13 @@
+package com.jianhui_zhu.openuseragent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
