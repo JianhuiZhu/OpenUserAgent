9e5940f1e07ff8abee6f49780209df271aad218e
jianhuizhu <jianhuizhu1987@gmail.com>
2016-02-10 11:22:10 -0500

    refactor view

--- app/src/main/java/com/jianhui_zhu/openuseragent/view/HomeView.java
+++ app/src/main/java/com/jianhui_zhu/openuseragent/view/HomeView.java
@@ -31,7 +31,7 @@ public class HomeView extends AbstractFragment implements HomeViewInterface{
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view= inflater.inflate(R.layout.fragment_home,container,false);
+        View view= inflater.inflate(R.layout.view_home,container,false);
         ButterKnife.bind(this, view);
         return view;
     }
--- app/src/main/java/com/jianhui_zhu/openuseragent/view/TestView.java
+++ app/src/main/java/com/jianhui_zhu/openuseragent/view/TestView.java
@@ -64,7 +64,7 @@ public class TestView extends AbstractFragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.fragment_test,container,false);
+        View view=inflater.inflate(R.layout.view_test,container,false);
         ButterKnife.bind(this,view);
         return view;
     }
