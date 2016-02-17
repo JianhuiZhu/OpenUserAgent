d22c37dbd9efaef87bb833540c174f8d1be6bcb9
jianhuizhu <jianhuizhu1987@gmail.com>
2016-02-16 21:40:01 -0500

    create class necessary

---
+++ app/src/main/java/com/jianhui_zhu/openuseragent/view/HistoryView.java
@@ -0,0 +1,7 @@
+package com.jianhui_zhu.openuseragent.view;
+
+/**
+ * Created by jianhuizhu on 2016-02-16.
+ */
+public class HistoryView {
+}
---
+++ app/src/main/java/com/jianhui_zhu/openuseragent/view/adapter/HistoryAdapter.java
@@ -0,0 +1,7 @@
+package com.jianhui_zhu.openuseragent.view.adapter;
+
+/**
+ * Created by jianhuizhu on 2016-02-16.
+ */
+public class HistoryAdapter {
+}
